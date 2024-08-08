140af546fb939f4896c2b63f63f2cfc5c6f510ee
sourabh singh bais <sourabhsinghbais52@gmail.com>
2024-08-09 00:04:42 +0530

    updated

--- src/components/newarrivals/Ndata.js
+++ src/components/newarrivals/Ndata.js
@@ -2,32 +2,32 @@ const Ndata = [
   {
     cover: "./images/arrivals/arrivals1.png",
     name: "Sunglass",
-    price: "150",
+    price: "1500",
   },
   {
     cover: "./images/arrivals/arrivals2.png",
     name: "Makeup",
-    price: "250",
+    price: "25000",
   },
   {
     cover: "./images/arrivals/arrivals3.png",
     name: "Smart Watch",
-    price: "50",
+    price: "5000",
   },
   {
     cover: "./images/arrivals/arrivals4.png",
     name: "Lipstick",
-    price: "15",
+    price: "150",
   },
   {
     cover: "./images/arrivals/arrivals5.png",
     name: "Green Plant",
-    price: "10",
+    price: "1000",
   },
   {
     cover: "./images/arrivals/arrivals6.png",
     name: "Bonsai tree",
-    price: "400",
+    price: "4000",
   },
 ]
 
